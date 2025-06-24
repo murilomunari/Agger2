c94f30f43fb5ccd20126347d4c66e8b9bb2d173a
murilomunari <munarimurilo441@gmail.com>
2025-06-24 17:34:27 -0300

    First commit

---
+++ src/main/java/com/agger/Agger2Application.java
@@ -0,0 +1,13 @@
+package com.agger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Agger2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Agger2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/agger/Agger2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Agger2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
